f3a1256da1ed2fd25ce4d43f4a1f4c51c5c609ce
technic404 <technic.cup@gmail.com>
2026-03-05 14:01:39 +0100

    Fulfill typedefs

--- src/main/java/pl/technic404/cjs/Constants.java
+++ src/main/java/pl/technic404/cjs/Constants.java
@@ -6,8 +6,22 @@ import java.util.function.Function;
 public class Constants {
 
     static final List<Function<String, List<String>>> METHODS = List.of(
+            (e) -> List.of("forms", ": CjsForm[]"),
+            (e) -> List.of("components", ": CjsComponentsCollection"),
+            (e) -> List.of("toForms", "(element: HTMLElement): CjsForm[]"),
             (e) -> List.of("withData", "(data: " + e + "): this"),
-            (e) -> List.of("render", "(data: " + e + "): this"),
-            (e) -> List.of("visualise", "(data: " + e + "): this")
+            (e) -> List.of("withStyle", "(style: CjsStyleProperties): this"),
+            (e) -> List.of("render", "(data: " + e + "): string"),
+            (e) -> List.of("visualise", "(data: " + e + "): HTMLElement"),
+            (e) -> List.of("setData", "(data: " + e + "): this"),
+            (e) -> List.of("loadLayout", "(...layouts: CjsLayout[]): this"),
+            (e) -> List.of("rerenderOnSearch", "(data: { useSmartRender: boolean }): this"),
+            (e) -> List.of("rerenderComponents", "(data: " + e + ", options: { useSmartRender: boolean }): this"),
+            (e) -> List.of("onLoad", "(callback: () => void): void"),
+            (e) -> List.of("hide", "(): void"),
+            (e) -> List.of("show", "(): void"),
+            (e) -> List.of("setDefaultData", "(data: " + e + "): this"),
+            (e) -> List.of("querySelector", "(...selectors: string[]): HTMLElement|null|HTMLElement[]"),
+            (e) -> List.of("querySelectorAll", "(...selectors: string[]): HTMLElement[]|Element[]")
     );
 }
